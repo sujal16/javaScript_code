bcabd47655fc3bc7f09eaac77c53c6f58297f17d
Sujal Varshney <86117482+sujal16@users.noreply.github.com>
2024-01-23 06:09:21 +0000

    java6th

---
+++ .devcontainer/java6th.js
@@ -0,0 +1,14 @@
+let x = 5
+// console.log(typeof x)//it gives output as a number so this is the function by which we can know
+//about the variable
+//mostly asked interview question
+// console.log(typeof null)//give output as a ***object***
+// console.log(typeof undefined)//give output as a unefined
+
+// to convert the one data type to another data 
+let stringNumber = String(x)
+console.log(typeof stringNumber)
+var someString = "33ab"
+let number = Number(someString)
+console.log(typeof number)
+console.log(number)//it is giving you an output as a not a number(NaN)
